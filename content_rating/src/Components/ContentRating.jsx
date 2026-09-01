92a3c0d952f2dd2fdc9baf92c6f7d60ec12e798c
harsh-k-117 <kulkarniharsh117@gmail.com>
2026-09-01 18:41:28 +0530

    Fixed repository issues and previously added code to ensure working event handlers

--- content_rating/src/Components/ContentRating.jsx
+++ content_rating/src/Components/ContentRating.jsx
@@ -2,15 +2,44 @@
 import React, { Component } from 'react';
 import './ContentRating.css';
 
-class ContentRating extends Component {
+class ContentRating extends React.Component {
   constructor() {
     super();
+    this.state = {
+      likes: 0,
+      dislikes: 0
+    };
   }
+
+  handleLike = () => {
+    this.setState((prevState) => ({
+      likes: prevState.likes + 1
+    }));
+  };
+
+  handleDislike = () => {
+    this.setState((prevState) => ({
+      dislikes: prevState.dislikes + 1
+    }));
+  };
+
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+      <>
+        <div>
+          <p>
+            Black Coffee is good for health. It can help improve focus, boost metabolism, and provide antioxidants.
+          </p>
+          <div className="rating-buttons">
+            <button className="like-button" onClick={this.handleLike}>
+              Like ({this.state.likes})
+            </button>
+            <button className="dislike-button" onClick={this.handleDislike}>
+              Dislike ({this.state.dislikes})
+            </button>
+          </div>
+        </div>
+      </>
     );
   }
 }
